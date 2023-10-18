28f3dde16cf14bda1727eb4650cd085bd1c114ff
Celil Ozturk <celil.ozturk@windowslive.com>
2023-10-18 20:11:35 +0300

    Nodejs packages, mongodb connection and create database

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
-console.log("Merhaba");
+const mongoose=require("mongoose");
+const express=require("express");
+const app=express();
+const {v4:uuidv4} =require("uuid");
+const multer=require("multer");
+const cors=require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+
+
+const uri="mongodb+srv://MongoDb:Abc-123@reactecommercedb.bxkaaz1.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri).then(res=>{
+    console.log("Database baglantisi basarili!");
+}).catch(err=>{
+    console.log(err.message)
+});
+
+
+const port=5000;
+app.listen(5000,()=>{
+    console.log("Uygulama http://localhost:"+port+" uzerinden ayakta!");
+});
+
+
